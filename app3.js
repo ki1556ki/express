8993e9758e0f2f07fb1b7ba53ff6af3c986944a1
ki1556ki <ki1556ki@naver.com>
2019-04-15 14:56:54 +0900

    express

---
+++ app3.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res, next) {
+	console.log('천번째 미들웨어 호출됨.')
+	
+	req.user = 'mike';
+	
+	next();
+});
+
+app.use(function(req, res, next) {
+	console.log('두번째 미들웨어 호출됨.');
+	
+	res.writeHead(200, {"Content-Type":"text/html;json;charset=utf8"});
+	res.end('<h1>서버에서 응답한 결과입니다 : ' + req.user + '</h1>');
+	
+});
+
+
+var server = http.createServer(app).listen(app.get('port'), function() {
+	console.log('익스프레스로 웹 서버를 실행함 : ' + app.get('port'));
+});
---
+++ app4.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res, next) {
+	console.log('천번째 미들웨어 호출됨.')
+	
+	req.user = 'mike';
+	
+	next();
+});
+
+app.use(function(req, res, next) {
+	console.log('두번째 미들웨어 호출됨.');
+	
+	//res.send('<h1>서버에서 응답한결과입니다 : ' + req.user + '</h1>');
+	var person = {name:'소녀시대', age:20};
+	
+	//res.send(person);
+	
+	var personStr = JSON.stringify(person);
+	//res.send(personStr);
+	
+	res.writeHead(200, {"Content-Type":"application/json;charset=utf8"});
+	res.write(personStr);
+	res.end();
+	
+});
+
+
+var server = http.createServer(app).listen(app.get('port'), function() {
+	console.log('익스프레스로 웹 서버를 실행함 : ' + app.get('port'));
+});
